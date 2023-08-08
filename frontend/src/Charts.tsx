97a077002db868fb6e6596267fabd25c53d12ebc
Matheus Dussin Bampi <matheusbampi@gmail.com>
2023-08-08 15:07:26 +0200

    feat: make properties independent on frontend

--- frontend/src/Charts.tsx
+++ frontend/src/Charts.tsx
@@ -8,33 +8,64 @@ interface ChartsProps {
 }
 
 const Charts: React.FC<ChartsProps> = ({ observations }) => {
-    // Convert the data to the format needed by recharts
-    const chartData = observations.map((obs: TObservation) => ({
-        timestamp: new Date(obs.timestamp).toLocaleTimeString(),
-        value: parseFloat(obs.value)
-    }));
+    const observationTypes = Array.from(new Set(observations.map((obs: TObservation) => obs.name)));
 
     return (
         <div>
             <h2>Observations</h2>
 
-            <h4>Heart Rate</h4>
+            {observationTypes.map((type) => {
+                const filteredObservations = observations.filter((obs: TObservation) => obs.name === type);
+                return <PropertyObservations
+                    property={type}
+                    observations={filteredObservations}
+                />;
+            })}
+        </div>
+    );
+}
 
-            <LineChart
-                width={500}
-                height={300}
-                data={chartData}
-                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-            >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="timestamp" />
-                <YAxis />
-                <Tooltip />
-                <Line type="monotone" dataKey="value" stroke="#82ca9d" />
-            </LineChart>
+interface PropertyObservationsProps {
+    property: string;
+    observations: TObservation[];
+}
 
-            {/* List with all obvervations */}
-            <table className="observations-table">
+const PropertyObservations: React.FC<PropertyObservationsProps> = ({ property, observations }) => {
+    const chartData = observations.map((obs: TObservation) => ({
+        timestamp: new Date(obs.timestamp).toLocaleTimeString(),
+        value: parseFloat(obs.value)
+    }));
+
+    return <div key={property} className='property-header'>
+
+        <h2>{property}</h2>
+        <p>Sensor: {observations[0].sensor} </p>
+        <p>Number of observations: {observations.length}</p>
+        <p>Min value: {Math.min(...observations.map((obs: TObservation) => parseFloat(obs.value)))}</p>
+        <p>Max value: {Math.max(...observations.map((obs: TObservation) => parseFloat(obs.value)))}</p>
+
+        <LineChart
+            width={700}
+            height={300}
+            data={chartData}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="timestamp" />
+            <YAxis dataKey="value" />
+            <Tooltip />
+            <Line type="monotone" dataKey="value" stroke="#82ca9d" />
+        </LineChart>
+
+        {/* List with all observations of current type */}
+        <ObservationsTable observations={observations} />
+    </div>
+}
+
+const ObservationsTable: React.FC<ChartsProps> = ({ observations }) => {
+    return (
+        <div>
+            <table className='observations-table'>
                 <thead>
                     <tr>
                         <th>Date</th>
@@ -43,8 +74,8 @@ const Charts: React.FC<ChartsProps> = ({ observations }) => {
                     </tr>
                 </thead>
                 <tbody>
-                    {observations.map((obs: TObservation, index: number) => (
-                        <tr key={index}>
+                    {observations.map((obs: TObservation) => (
+                        <tr key={obs.timestamp}>
                             <td>{new Date(obs.timestamp).toLocaleDateString()}</td>
                             <td>{new Date(obs.timestamp).toLocaleTimeString()}</td>
                             <td>{obs.value}</td>
